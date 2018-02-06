b404630c8528147bfaf5f828b02c1e767e0a127c
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-02-06 02:52:20 -0800

    Create rm4.c

---
+++ rm4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+ 
+    if(number % 4 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    getch();
+}
+© 2018 GitHub, Inc.
