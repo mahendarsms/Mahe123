b32d55a073e757fbf57b0323245f718ff2691914
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-02-06 02:27:03 -0800

    Create 9.C

---
+++ 9.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) {
+	int a[10000],n,k,sum;
+	sum=0;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	scanf("%d",&k);
+	for(int j=0;j<k;j++)
+	{
+		sum=sum+a[j];
+	}
+	
+printf("%d",sum);
