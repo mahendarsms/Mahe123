6b31cecd59d6f95fbae7aeef5d7d58b4a2452f13
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-02-09 16:17:10 +0530

    Update 3.3.c

--- 3.3.c
+++ 3.3.c
@@ -1,11 +1,25 @@
-num=int(input())
-if num>1:
-	for i in range(2,num):
-		if(num%i==0):
-			print(num,"the number is not prime")
-			break
-	else;
-			print(num,"the number is  prime")
-  else;
-	print("the number is not prime")
+#include<stdio.h>
+void main()
+{
+	int a,b,max=0,gcd=0,i,lcm;
+	printf("ENTER THE TWO INTEGERS :\n");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		max=a;
+	}
+	else
+	{
+		max=b;
+	}
+	for(i=2;i<max;i++)
+	{
+		if(((a%i)==0)&&((b%i)==0))
+		{
+			gcd=i;
+		}
+	lcm=(a*b)/gcd;
+	printf("THE LCM FOR %d AND %d IS %d",a,b,lcm);
+}
+
 
