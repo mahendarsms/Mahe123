2e19370c6ae55d13ad5cb1a543964ed8eb2c15c1
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-02-09 16:26:52 +0530

    Create 3.5.c

---
+++ 3.5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+	// your code goes here
+	int i, n,m;
+	scanf("%d %d",&n,&m);
+	for(i=n;i<=m;i++)
+	{
+		printf("%c\t",i);
+	}
+	
+  return0;
+}
