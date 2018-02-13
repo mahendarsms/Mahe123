dbb61d636766ba6b6e2e8e2b616c6f4d61f6703c
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-02-13 16:31:54 +0530

    Update 4.2.c

--- 4.2.c
+++ 4.2.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
-#include<string.h>
-int main(void) {
-	char a[50];
-	int i,count=1;
-	gets(a);
-	
-	for(i=0;a[i]!='\0';i++)
-  {
-		if(a[i] == ' ')
-		count++;
-		
-		
-  }
+#include<stdio.h>
+void main()
+{
+	int i=0,count=0;
+	char str[30];
+	printf("Enter the String:");
+	scanf("%s",&str);
+	while(str[i]!='\0')
+	{
+		if(str[i]=="[^n]")
+		{
+			++count; 
+		}
+		++i;
+	}
+	++count;
 	printf("%d",count);
-	return 0;
 }
