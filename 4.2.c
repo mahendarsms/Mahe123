5ade731b4e5e658d085c5f658a8516cc658968d6
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-02-13 16:24:45 +0530

    Update 4.2.c

--- 4.2.c
+++ 4.2.c
@@ -1,17 +1,17 @@
-include<stdio.h>
-void main()
-{
-char str[30];
-int i=0,count=0;
-  printf("enter the string");
-  scanf("%s",&str);
-while(str[i]!='\0')
-{
-if(!(str[i]==" "))
-{
-++count;
-}
-++i;
-}
-printf("no of characters is %d ",count);
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[50];
+	int i,count=1;
+	gets(a);
+	
+	for(i=0;a[i]!='\0';i++)
+  {
+		if(a[i] == ' ')
+		count++;
+		
+		
+  }
+	printf("%d",count);
+	return 0;
 }
