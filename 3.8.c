26df598e0e6fa152ea588ffcd6cceec7e8a888bd
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-02-13 15:46:10 +0530

    Create 3.8.c

---
+++ 3.8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,a[10];
+	printf("ENTER THE TOTAL NO.OF.ELEMENTS:\n");
+	scanf("%d",&n);
+	printf("ENTER THE ELEMENTS:\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf(" THE ELEMENTS WITH ITS INDEX POSITION ARE:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d\t%d",a[i],i);
+	}
