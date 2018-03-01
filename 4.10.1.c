cda8d709cdd65ae3522e8da631b7e742b4568ca0
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-03-01 16:35:26 +0530

    Create 4.10.1.c

---
+++ 4.10.1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a=0,b=1,c,n;
+	printf("ENTER THE NO.OF.ELEMENTS:\n");
+	scanf("%d",&n);
+	printf("%d",b);
+	while(n!=0)
+	{
+		c=a+b;
+		printf("\n%d",c);
+		a=b;
+		b=c;
+		n--;
+	}
+  }
