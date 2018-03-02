934218552050beef452203d48ab576b899815542
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-03-02 15:07:58 +0530

    Create 7.9.c

---
+++ 7.9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+    printf("enter the number:");
+    scanf("%d %d",&a,&b);
+    c=a-b;
+    if(c%2==0)
+    {
+
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+}
