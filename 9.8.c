728ca7c074b816a2f0f2d2bb71d66d8a271b0053
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-03-13 16:31:17 +0530

    Update 9.8.c

--- 9.8.c
+++ 9.8.c
@@ -5,12 +5,12 @@ int main(void)
 	scanf("%d %d",&n1,&n2);
 	lcm=(n1>n2)?n1:n2;
 	while(1)
-	{
+{
 		if(lcm%n1==0 && lcm%n2==0)
-		{
+{
 			printf("\nlcm=%d",lcm);
 			break;
-		}
+}
 	++lcm;
 	}
 	return 0;
